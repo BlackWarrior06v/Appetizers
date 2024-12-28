384f3ef18fe5595bf4a5be4f245a418f47ea550e
BlackWarrior06v <curipch2@gmail.com>
2024-12-27 19:31:22 -0600

    Initial Commit

---
+++ Apetizers/ApetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApetizersApp.swift
+//  Apetizers
+//
+//  Created by Curi on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ApetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApetizersTests/ApetizersTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApetizersTests.swift
+//  ApetizersTests
+//
+//  Created by Curi on 27/12/24.
+//
+
+import Testing
+@testable import Apetizers
+
+struct ApetizersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
